fa8d07c56946ffca8122ef170af7330b6df4c9d8
aliyarahman <aliya.rahman@yahoo.com>
2015-08-02 18:49:25 -0400

    Added README file

---
+++ README.md
@@ -0,0 +1,13 @@
+D3 tutorials you've probably already seen, commented for learners
+================================================================
+
+As I've thought more about data as a story-telling process for social justice work, I've learned and written a lot of d3. Because someone usually needs to move fast on a civil rights campaign or an event or a training, I've almost always quickly hacked my way through to generate visualizations that work well and look good - but that are barely commented and as such not too useful when I come back to reuse for a slightly (or entirely) different need.
+
+My sensei used to tell me this story (which felt like a horror story, at the time) of the karateka who got a black belt and them promptly discarded it to go back to the beginning and train again from white belt, now with ten years more knowledge. When I got old enough I tried that in the dojo. It did amazing things for my technical skill and understanding. So I'm trying that in code land.
+
+I'm not making up and posting new tutorials (yet) - there are plenty of those, and I actually think the d3.js site actually lists them in an order that works well for a beginner's syllabus. Instead, I'm redoing some of the best tutorials with good commenting that should stand alone to explain the code when I come back to it (and that explains some possible stuck points that aren't totally explained in the tutorials themselves - like how to serve external files on your local computer if you want to pull data from a csv or tsv or json file without security errors).
+
+So far, I've included code and comments for:
+
+00bar_chart: Mike Bostock's tutorial on basic bar charts, parts I and II (just horizontal bars). http://bost.ocks.org/mike/bar/1/
+01bar_chart * not done: Mike Bostock's tutorial on bar charts, part III (vertical bars, and adds labels) http://bost.ocks.org/mike/bar/3/
--- js/01bar_chart_with_labels.js
+++ js/01bar_chart_with_labels.js
@@ -1,2 +1,40 @@
 /* Code for an SVG driven d3 bar chart with labels
 /*===============================================*/
+
+/* Note - read the header for 00bar_chart.js to learn how to serve an external data file from a local computer.
+   Otherwise you'll get errors and nothing will show up on your page. */
+
+
+// Again, the stuff in the first three chunks of code are all things that don't depend on the data, so they can happen outside of the tsv loader.
+var width = 960;
+var height = 500;
+
+var y = d3.scale.linear()
+    .range([height, 0]);  // Width and heigh flip from the previous exercise because we're rotating the bars to go up and down rather than left to right
+
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", height);
+
+// Everything else depends on the values of the data, so we'll encapsulate all of that code inside the tsv loader
+d3.tsv("http://localhost:8888/data/00barchartdata.tsv", type function(error, data) {
+    y.domain([0, d3.max(data function(d) { return d.value; })]); // We flipped the bars, so now we have to do y's domain, not x's
+
+    var barWidth = width / data.length; // Before we could fix the width of the bars and use that to calculate the height of the chart, but since we've flipped axes now we have to start from the width of the chart and move backwards
+
+    var bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d,i) { return "translate(" + i * barWidth + ",0)";});
+
+    bar.append("rect")
+        .attr("y", function(d) {return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value;) }) // SVG's coordinates start from the top left of the screen - whereas mathematical charts like this one more commonly start from the bottom left. So we have to do a conversion here for the y coordinate.
+        .attr("width"), barWidth - 1);
+
+    bar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", function(d) { return y(d.value) + 3 })
+        .attr("dy", ".75em") // This becomes different here because of the axis flipping - before we were anchoring the text at its base and now we are anchoring at its cap
+        .text(function(d) { return d.value });
+});
